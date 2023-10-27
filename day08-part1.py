f6e11b034c3c1a84a7faf81ced03ebfecfd4e0b9
Sam Malayek <malayek@gmail.com>
2023-10-27 02:12:39 -0700

    Simpler solution for day08

--- day08-part1.py
+++ day08-part1.py
@@ -1,21 +1,14 @@
+from collections import defaultdict
+
 
 def main():
     lines = open('day08.txt', 'r').read().splitlines()
-    register = {}
-
-    # Required (rather than using defaultdict) to handle conditional statements that are exec'd
-    for l in lines:
-        key, _ = l.split(maxsplit=1)
-        register[key] = 0
+    register = defaultdict(int)
 
     for l in lines:
-        key, rest = l.replace('inc ', '+= (').replace('dec ', '-= (').split(maxsplit=1)
-        for curKey in register.keys():
-            rest = rest.replace(f' {curKey} ', f' register["{curKey}"] ')
+        toExec = l.replace('inc ', '+= ').replace('dec ', '-= ') + ' else 0'
 
-        toExec = f'register["{key}"] {rest} else 0)'
-
-        exec(toExec, {}, {'register': register})
+        exec(toExec, {}, register)
 
     print(max(register.values()))
 
--- day08-part2.py
+++ day08-part2.py
@@ -1,22 +1,15 @@
+from collections import defaultdict
+
 
 def main():
     lines = open('day08.txt', 'r').read().splitlines()
-    register = {}
+    register = defaultdict(int)
     values = []
 
-    # Required (rather than using defaultdict) to handle conditional statements that are exec'd
     for l in lines:
-        key, _ = l.split(maxsplit=1)
-        register[key] = 0
+        toExec = l.replace('inc ', '+= ').replace('dec ', '-= ') + ' else 0'
 
-    for l in lines:
-        key, rest = l.replace('inc ', '+= (').replace('dec ', '-= (').split(maxsplit=1)
-        for curKey in register.keys():
-            rest = rest.replace(f' {curKey} ', f' register["{curKey}"] ')
-
-        toExec = f'register["{key}"] {rest} else 0)'
-
-        exec(toExec, {}, {'register': register})
+        exec(toExec, {}, register)
 
         values.append(max(register.values()))
 
