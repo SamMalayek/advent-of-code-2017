6477c7eca8dd1dafea1dff6c0ce6e4abec03a279
Sam Malayek <malayek@gmail.com>
2023-10-27 01:24:48 -0700

    Update day08

--- day08-part1.py
+++ day08-part1.py
@@ -3,27 +3,18 @@ def main():
     lines = open('day08.txt', 'r').read().splitlines()
     register = {}
 
-    # eval() does not like undefined dict keys, even if using defaultdict.
-    # It must only be used for evaluating expressions.
     for l in lines:
         key, _ = l.split(maxsplit=1)
         register[key] = 0
 
     for l in lines:
-        key, operator, rest = l.split(maxsplit=2)
+        key, rest = l.replace('inc ', '+= (').replace('dec ', '-= (').split(maxsplit=1)
         for curKey in register.keys():
             rest = rest.replace(f' {curKey} ', f' register["{curKey}"] ')
 
-        toEval = f'({rest} else 0)'
+        toEval = 'register["' + key + '"] ' + rest + ' else 0)'
 
-        # eval() does not like += and -= because they are augmented assignment statements.
-        # It must only be used for evaluating expressions.
-        if operator == 'inc':
-            register[key] += eval(toEval)
-        else:
-            register[key] -= eval(toEval)
-
-        eval(toEval, {}, {'register': register})
+        exec(toEval, {}, {'register': register})
 
     print(max(register.values()))
 
--- day08-part2.py
+++ day08-part2.py
@@ -4,27 +4,19 @@ def main():
     register = {}
     values = []
 
-    # eval() does not like undefined dict keys, even if using defaultdict.
-    # It must only be used for evaluating expressions.
     for l in lines:
         key, _ = l.split(maxsplit=1)
         register[key] = 0
 
     for l in lines:
-        key, operator, rest = l.split(maxsplit=2)
+        key, rest = l.replace('inc ', '+= (').replace('dec ', '-= (').split(maxsplit=1)
         for curKey in register.keys():
             rest = rest.replace(f' {curKey} ', f' register["{curKey}"] ')
 
-        toEval = f'({rest} else 0)'
+        toEval = 'register["' + key + '"] ' + rest + ' else 0)'
 
-        # eval() does not like += and -= because they are augmented assignment statements.
-        # It must only be used for evaluating expressions.
-        if operator == 'inc':
-            register[key] += eval(toEval)
-        else:
-            register[key] -= eval(toEval)
+        exec(toEval, {}, {'register': register})
 
-        eval(toEval, {}, {'register': register})
         values.append(max(register.values()))
 
     print(max(values))
