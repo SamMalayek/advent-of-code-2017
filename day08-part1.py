00b5f18659b2c4c34b5736a3fb30cb0840c029fe
Sam Malayek <malayek@gmail.com>
2023-10-27 01:03:10 -0700

    Add day08

---
+++ day08-part1.py
@@ -0,0 +1,32 @@
+
+def main():
+    lines = open('day08.txt', 'r').read().splitlines()
+    register = {}
+
+    # eval() does not like undefined dict keys, even if using defaultdict.
+    # It must only be used for evaluating expressions.
+    for l in lines:
+        key, _ = l.split(maxsplit=1)
+        register[key] = 0
+
+    for l in lines:
+        key, operator, rest = l.split(maxsplit=2)
+        for curKey in register.keys():
+            rest = rest.replace(f' {curKey} ', f' register["{curKey}"] ')
+
+        toEval = f'({rest} else 0)'
+
+        # eval() does not like += and -= because they are augmented assignment statements.
+        # It must only be used for evaluating expressions.
+        if operator == 'inc':
+            register[key] += eval(toEval)
+        else:
+            register[key] -= eval(toEval)
+
+        eval(toEval, {}, {'register': register})
+
+    print(max(register.values()))
+
+
+if __name__ == "__main__":
+    main()
