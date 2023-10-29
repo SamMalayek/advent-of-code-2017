cfd0279ad6f158baa56587a7065a7588d56aaf66
Sam Malayek <malayek@gmail.com>
2023-10-29 00:14:00 -0700

    Add day10

---
+++ day10-part1.py
@@ -0,0 +1,35 @@
+
+def main():
+    lengths = list(map(int, open('day10.txt', 'r').read().rstrip().split(',')))
+
+    sequence = [i for i in range(256)]
+    n = len(sequence)
+    i = 0
+    skipSize = 0
+
+    for length in lengths:
+        if length > n:
+            continue
+        # Reverse the order of that length of elements,
+        # starting with the element at i.
+        # Note that this reversing is circular.
+        end = (i + length) % n
+        if i < end:
+            sequence = sequence[:i] + sequence[i:i+length][::-1] + sequence[length+i:]
+        elif i == end:
+            pass
+        else:
+            wrappedSublist = sequence[i:] + sequence[:end]
+            wrappedSublist.reverse()
+            sequence[:end], sequence[i:] = wrappedSublist[n-i:], wrappedSublist[:n-i]
+
+        # Move i forward by length + skip size
+        i = (i + length + skipSize) % n
+
+        # Increase skip size by 1
+        skipSize += 1
+
+    print(sequence[0] * sequence[1])
+
+
+main()
---
+++ day10-part2.py
@@ -0,0 +1,45 @@
+from functools import reduce
+
+def main():
+    lengths = [ord(c) for c in open('day10.txt', 'r').read().rstrip()]
+    lengths.extend([17, 31, 73, 47, 23])
+
+    sequence = [i for i in range(256)]
+    n = len(sequence)
+    i = 0
+    skipSize = 0
+
+    for _ in range(64):
+        for length in lengths:
+            if length > n:
+                continue
+            # Reverse the order of that length of elements,
+            # starting with the element at i.
+            # Note that this reversing is circular.
+            end = (i + length) % n
+            if i < end:
+                sequence = sequence[:i] + sequence[i:i+length][::-1] + sequence[length+i:]
+            elif i == end:
+                pass
+            else:
+                wrappedSublist = sequence[i:] + sequence[:end]
+                wrappedSublist.reverse()
+                sequence[:end], sequence[i:] = wrappedSublist[n-i:], wrappedSublist[:n-i]
+
+            # Move i forward by length + skip size
+            i = (i + length + skipSize) % n
+
+            # Increase skip size by 1
+            skipSize += 1
+
+    knotHashNums = []
+
+    for i in range(0, len(sequence), 16):
+        knotHashNums.append(reduce(lambda i, j: i ^ j, sequence[i:i+16]))
+
+    knotHash = ''.join([hex(d)[2:].rjust(2, '0') for d in knotHashNums])
+
+    print(knotHash)
+
+
+main()
