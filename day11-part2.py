4c59aa56beaf90f3ce21ebe96c5143c1b1798eea
Sam Malayek <malayek@gmail.com>
2023-11-04 21:11:08 -0700

    Update day11 part2

--- day11-part2.py
+++ day11-part2.py
@@ -15,22 +15,17 @@ def main():
             return x-1, y, z+1
         return x, y-1, z+1  # nw
 
-    def getDistance(toX, toY, toZ):
-        return (abs(toX) + abs(toY) + abs(toZ)) / 2
-
-    furthestDist = 0
+    def getSteps(toX, toY, toZ):
+        return max([abs(toX), abs(toY), abs(toZ)])
 
     x, y, z = 0, 0, 0
-    goalX, goalY, goalZ = 0, 0, 0
+    furthestDist = 0
 
     for d in dirs:
         x, y, z = getNeighbor(x, y, z, d)
-        curDist = getDistance(x, y, z)
-        if curDist > furthestDist:
-            goalX, goalY, goalZ = x, y, z
-            furthestDist = curDist
+        furthestDist = max(furthestDist, getSteps(x, y, z))
 
-    print(max([abs(goalX), abs(goalY), abs(goalZ)]))
+    print(furthestDist)
 
 
 if __name__ == "__main__":
