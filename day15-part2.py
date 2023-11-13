b72b33f1180cef1ab27723ee18b48e45a1577fee
Sam Malayek <malayek@gmail.com>
2023-11-12 21:27:21 -0800

    Update day15

--- day15-part2.py
+++ day15-part2.py
@@ -4,14 +4,14 @@ def main():
     count = 0
     divisor = 2147483647
 
-    def gen(multi, factor, cur):
+    def createGen(multi, factor, cur):
         while True:
             cur = (cur * multi) % divisor
             if cur % factor == 0:
                 yield cur
 
-    yieldA = gen(16807, 4, int(lines[0].split()[-1]))
-    yieldB = gen(48271, 8, int(lines[1].split()[-1]))
+    yieldA = createGen(16807, 4, int(lines[0].split()[-1]))
+    yieldB = createGen(48271, 8, int(lines[1].split()[-1]))
     for _ in range(5000000):
         a = next(yieldA)
         b = next(yieldB)
