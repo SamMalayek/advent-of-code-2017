2429d7496c7860aa9421bbfa396331352279fe5e
Sam Malayek <malayek@gmail.com>
2023-11-19 02:13:49 -0800

    Add day18 part1

---
+++ day18-part1.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+
+
+def main():
+    cmds = open('day18.txt').read().splitlines()
+    register = defaultdict(int)
+    playedSounds = []
+    recoveredSounds = []
+
+    def isNum(val):
+        try:
+            int(val)
+            return True
+        except ValueError:
+            return False
+
+    def getNum(elem):
+        return int(elem) if isNum(elem) else register[elem]
+
+    cur = 0
+
+    while cur < len(cmds):
+        cmd = cmds[cur]
+        op, rest = cmd.split(' ', 1)
+        rest = rest.split()
+        if op == 'snd':
+            num = getNum(rest[0])
+            playedSounds.append(num)
+        elif op == 'set':
+            num = getNum(rest[1])
+            register[rest[0]] = num
+        elif op == 'add':
+            num = getNum(rest[1])
+            register[rest[0]] += num
+        elif op == 'mul':
+            num = getNum(rest[1])
+            register[rest[0]] *= num
+        elif op == 'mod':
+            num = getNum(rest[1])
+            if num != 0:
+                register[rest[0]] %= num
+        elif op == 'rcv':
+            if len(playedSounds) != 0 and getNum(rest[0]) != 0:
+                val = playedSounds.pop()
+                recoveredSounds.append(val)
+                print(recoveredSounds[-1])
+                quit()
+        elif op == 'jgz':
+            if getNum(rest[0]) > 0:
+                cur += getNum(rest[1])
+                continue
+
+        cur += 1
+
+
+if __name__ == "__main__":
+    main()
